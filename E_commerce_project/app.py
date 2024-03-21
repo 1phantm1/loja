b6920268ff1e549e8ed235a73151afa6d2ac188c
1phantm1 <pietrodavila37@gmail.com>
2024-03-21 00:22:47 -0300

    a

--- E_commerce_project/app.py
+++ E_commerce_project/app.py
@@ -1,8 +1,34 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Email, Length, EqualTo
+from flask_bcrypt import Bcrypt
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="sk.env")
 
 app = Flask(__name__, template_folder='templates')
-app.config["Secret_key"] = "gN9a*K8f8@%7Ji"
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+bcrypt = Bcrypt(app)
+
+class RegistrationForm(FlaskForm):
+    name = StringField("Nome completo", validators=[InputRequired()])
+    mail = StringField("Email", validators=[InputRequired(), Email()])
+    password = PasswordField("Senha", validators=[InputRequired(), Length(min=8)])
+    confirm_pass = PasswordField("Confirmar senha", validators=[InputRequired(), EqualTo("password")])
+    submit = SubmitField("Criar conta")
+
+@app.route("/criar-conta.html", methods=["GET", "POST"])
+def criar_conta():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        mail = form.mail.data
+        password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+        session["email"] = mail
+        return redirect(url_for("index"))
+    return render_template("criar-conta.html", form=form)
 
 @app.route("/")
 def index():
